0918b1fd95e64da6149c8ee7d5338f8c1d63e11d
KennethC <91973026+KennethECochran@users.noreply.github.com>
2023-10-10 17:32:37 -0400

    Added boilerplate HTML, CSS, JavaScript files. Added placeholder books to index.html for vizualization.

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, numPages, read){
+    this.title = title
+    this.author = author
+    this.numPages = numPages
+    this.read = read
+}
+
+function addBookToLibrary{
+    
+}
